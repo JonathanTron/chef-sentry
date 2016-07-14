a52071db8d2946ead132ec81a5bbba3a24418e4d
Matt Leick <matt@mangohealth.com>
2016-07-14 15:28:43 -0700

    Update Chef-Sentry cookbook to support Sentry 8.6.0
    - templates
    add sentry.conf.8.0.py.erb template with structure accomodating to properties
    present in the generated sentry.conf.py file in 8.6
    add conf.yml file to conform to 8.x Sentry standards of splitting configs
    out from sentry.conf.py to conf.yml
    
    - attributes
    update attributes/default.rb to contain default values for new properties in
    sentry.conf.py and conf.yml
    
    - recipes
    update _configure.rb to check for Sentry version and use new templates if
    version > 8

--- attributes/default.rb
+++ attributes/default.rb
@@ -28,10 +28,19 @@ default["sentry"]["plugins"] = [
   ["django-bcrypt", "0.9.2"],
   ["django-sendmail-backend", "0.1.2"],
 ]
+# dependencies per: https://docs.getsentry.com/on-premise/server/installation/python/
 default["sentry"]["dependency"]["packages"] = [
+  "python-setuptools",
+  "python-pip",
+  "python-dev",
+  "gcc"
+  "libjpeg-dev",
   "libxml2-dev",
+  "libxslt-dev",
   "libxslt1-dev",
   "libffi-dev",
+  "libyaml-dev",
+  "libpq-dev", # not listed but blocks sentry install
 ]
 default["sentry"]["install_dir"] = "/opt/sentry"
 default["sentry"]["filestore_dir"] = "/opt/sentry/data"
@@ -44,6 +53,8 @@ default["sentry"]["env_path"] = "#{node["sentry"]["env_d_path"]}/env"
 default["sentry"]["config"]["db_engine"] = "django.db.backends.postgresql_psycopg2"
 default["sentry"]["config"]["db_options"] = {autocommit: true}
 default["sentry"]["config"]["admin_email"] = ""
+
+# web server
 default["sentry"]["config"]["allow_registration"] = false
 default["sentry"]["config"]["beacon"] = false
 default["sentry"]["config"]["public"] = false
@@ -57,18 +68,30 @@ default["sentry"]["config"]["web_options"] = {
   }
 }
 default["sentry"]["config"]["url_prefix"] = "http://localhost:#{node["sentry"]["config"]["web_port"]}"
+
+# smtp
+default["sentry"]["config"]["smtp_host"] = '0.0.0.0'
+default["sentry"]["config"]["smtp_port"] = '1025'
+default["sentry"]["config"]["smtp_hostname"] = 'localhost'  
+
 default["sentry"]["config"]["email_default_from"] = "#{node["sentry"]["user"]}@#{node[:fqdn]}"
 default["sentry"]["config"]["email_backend"] = "django.core.mail.backends.smtp.EmailBackend"
 default["sentry"]["config"]["email_host"] = "localhost"
 default["sentry"]["config"]["email_port"] = "25"
 default["sentry"]["config"]["email_use_tls"] = false
 default["sentry"]["config"]["email_subject_prefix"] = nil
+default["sentry"]["config"]["email_list_namespace"] = "localhost"
+default["sentry"]["config"]["email_enable_replies"] = false
 default["sentry"]["config"]["additional_apps"] = ["djangosecure", "django_bcrypt"]
 default["sentry"]["config"]["prepend_middleware_classes"] = ["djangosecure.middleware.SecurityMiddleware"]
 default["sentry"]["config"]["append_middleware_classes"] = []
+# general
 default["sentry"]["config"]["use_big_ints"] = true
+default["sentry"]["config"]["single_organization"] = true
+default["sentry"]["config"]["debug"] = false
 # Redis config
 default["sentry"]["config"]["redis_enabled"] = true
+default["sentry"]["config"]["redis_config"]["hosts"][0]["name"] = "default"
 default["sentry"]["config"]["redis_config"]["hosts"][0]["host"] = "127.0.0.1"
 default["sentry"]["config"]["redis_config"]["hosts"][0]["port"] = "6379"
 # Cache config
@@ -79,7 +102,8 @@ default["sentry"]["config"]["broker_url"] = "redis://localhost:6379"
 default["sentry"]["config"]["celeryd_concurrency"] = 1
 default["sentry"]["config"]["celery_send_events"] = false
 default["sentry"]["config"]["celerybeat_schedule_filename"] = "#{default["sentry"]["filestore_dir"]}/celery_beat_schedule"
-
+#digests config
+default["sentry"]["config"]["digests"] = 'sentry.digests.backends.redis.RedisBackend'
 default["sentry"]["config"]["ratelimiter"] = "sentry.ratelimits.redis.RedisRateLimiter"
 default["sentry"]["config"]["buffer"] = "sentry.buffer.redis.RedisBuffer"
 default["sentry"]["config"]["quotas"] = "sentry.quotas.redis.RedisQuota"
@@ -87,6 +111,8 @@ default["sentry"]["config"]["tsdb"] = "sentry.tsdb.redis.RedisTSDB"
 # Filestore config
 default["sentry"]["config"]["filestore"] = "django.core.files.storage.FileSystemStorage"
 default["sentry"]["config"]["filestore_options"]["location"] = default["sentry"]["filestore_dir"]
+# data sampling
+default["sentry"]["config"]["sample_data"] = false
 
 default["sentry"]["data_bag"] = "sentry"
 default["sentry"]["data_bag_item"] = "credentials"
